fcfe5b069a69255b21f036c1bfa004940dc5c680
PRISCILLA URIBE <puribe0@student.ubc.ca>
2023-08-15 14:00:39 -0400

    added create new and show routes and corresponding views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+  before_action :set_task, except: [:index, :new, :create]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = task.new(task_params)
+    @task.save
+    redirect_to task_path(@tasks)
+  end
 end
